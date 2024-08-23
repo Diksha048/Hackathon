06741c4797890a3a0f3db421986bf1aef9cc4f7f
Diksha048 <bhagwatdiksha0408@gmail.com>
2024-08-23 11:52:39 +0530

    images added

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,19 @@
 document.getElementById('dropdown-items').addEventListener('click', function() {
     document.getElementById('navMenu').classList.toggle('active');
 });
+
+/* Theme change */
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeIcon.addEventListener('click', () => {
+    if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeIcon.src = './images/moon.png';
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeIcon.src = './images//sun.png';
+    }
+});
